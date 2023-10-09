43efe8902f9cc3e74a000178888627d5209c0d8e
fborsani <fborsani96@gmail.com>
2023-10-10 00:14:57 +0200

    Deleted duplicate folder

--- JVM/Installer/Python/installRepo.sh
+++
@@ -1,9 +0,0 @@
-if [[ $UID != 0 ]]; then
-	echo "root privileges required. Insert password..."
-	sudo "$0" "$@"
-fi
-
-apt update && apt upgrade -y
-apt install software-properties-common -y
-add-apt-repository ppa:deadsnakes/ppa
-apt install python3.10
