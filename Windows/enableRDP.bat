5d7f0dc482a304ccf59f93d8f8c9c78a6fbf906f
Fabio Borsani <32748998+fborsani@users.noreply.github.com>
2023-10-12 10:18:55 +0200

    Add files via upload

---
+++ Windows/enableRDP.bat
@@ -0,0 +1,23 @@
+@echo OFF
+for /f "tokens=3 delims= " %%i in ('reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections') do (
+    if %ERRORLEVEL% equ 0 (
+		echo Missing key
+		set doWork=1
+	)
+	if "%%i" equ "0x1" (
+        echo RDP is active
+		set doWork=0
+    ) else (
+        echo RDP not active
+		set doWork=1
+    )
+)
+
+if %doWork% equ 1(
+	echo enabling RDP
+	reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f && (echo Registry configured) || (echo Registry config failed; exit)
+	netsh firewall add portopening TCP 3389 "Remote Desktop" && (echo Firewall configured) || (echo Firewall config failed; exit)
+	echo Done.
+)
+
+pause
